5e4ee6d7cf95c594f034da79e136bbb445483661
victormf123 <victormf.df@hotmail.com>
2017-03-23 17:52:13 -0300

    Adicionando alterações em requeridas no gcloud

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-var app = require('./config/custom-express')();
+/*var app = require('./config/custom-express')();
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, function () {
    console.log('Servidor rodando na porta '+PORT+' .');
+});*/
+
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+});
+
+const server = app.listen(8080, function () {
+        const host = server.address().address;
+const port = server.address().port;
+
+console.log("Example app listening at http://"+ host +":"+ port +"");
 });
 
